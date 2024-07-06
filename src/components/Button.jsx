da26a9c104f73a9f56a4e25791bfbbe9dbde2211
PUNIT SINGH BISHT <punitsinghbisht1529@gmail.com>
2024-07-06 13:35:05 +0530

    minor change

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,9 +1,9 @@
 import ButtonSvg from "../assets/svg/ButtonSvg";
+
 const Button = ({ className, href, onClick, children, px, white }) => {
   const classes = `button relative inline-flex items-center justify-center h-11 transition-colors hover:text-color-1 ${
     px || "px-7"
   } ${white ? "text-n-8" : "text-n-1"} ${className || ""}`;
-
   const spanClasses = "relative z-10";
 
   const renderButton = () => (
@@ -19,9 +19,8 @@ const Button = ({ className, href, onClick, children, px, white }) => {
       {ButtonSvg(white)}
     </a>
   );
-  return href
-    ? renderLink()
-    : renderButton();
+
+  return href ? renderLink() : renderButton();
 };
 
 export default Button;
--- src/components/CompanyLogos.jsx
+++ src/components/CompanyLogos.jsx
@@ -1,18 +1,23 @@
-import { companyLogos } from "../constants"
+import { companyLogos } from "../constants";
 
 const CompanyLogos = ({ className }) => {
   return (
     <div className={className}>
-      <h5 className="tagline mb-6 text-center text-n-1/50">HELPING PEOPLE CREATE BEAUTIFUL CONTENT AT</h5>
+      <h5 className="tagline mb-6 text-center text-n-1/50">
+        Helping people create beautiful content at
+      </h5>
       <ul className="flex">
-        {companyLogos.map(( logo,index ) => (
-          <li className="flex items-center justify-center h-[8.5rem] flex-1" key={index}>
+        {companyLogos.map((logo, index) => (
+          <li
+            className="flex items-center justify-center flex-1 h-[8.5rem]"
+            key={index}
+          >
             <img src={logo} width={134} height={28} alt={logo} />
           </li>
         ))}
       </ul>
     </div>
-  )
-}
+  );
+};
 
-export default CompanyLogos
+export default CompanyLogos;
